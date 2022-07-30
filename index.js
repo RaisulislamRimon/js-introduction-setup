e506876574a1e72b19718f4ba1a95f0d788b360c
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-07-30 07:13:48 +0000

    index.js update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-console.log(1);
-console.log(2);
-console.log(3);
-console.log(4);
-console.log(5);
+for(var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
